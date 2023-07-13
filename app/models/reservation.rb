306c9d721b085d96ff53f6ccb44a0cde884864c1
Harrison Njuguna <noqsnoqs@gmail.com>
2023-07-13 17:03:37 +0300

    Modify reservation model

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,9 @@
 class Reservation < ApplicationRecord
-end
+    belongs_to :user
+    belongs_to :laptop
+  
+    validates :city, presence: true
+    validates :duration_of_hire, presence: true
+    validates :date_of_reservation, presence: true
+  end
+  
