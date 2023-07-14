8fb2d20fa0a4732f660b75fb42d22e25167810a7
Harrison Njuguna <noqsnoqs@gmail.com>
2023-07-14 18:59:19 +0300

    Modify seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Laptop.create(name: "Lenovo Ideapad 3 Laptop", description: "Design features include a large touchpad for easier scrolling and navigation, a spill-resistant keyboard, and a clean and simple aesthetic style with a choice of a metallic or patterned textured finish.", image_url: "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/56/838354/1.jpg", price: 10000,model_year: "2021",  rom_size: "1tb", ram_size: "8gb")
+Laptop.create(name: "DELL Latitude 7280", description: "The Dell Latitude 7280 runs smoothly and cranks through basic tasks just fine. On the other hand, it’s super tough.", image_url: "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/77/216369/1.jpg", price: 440000, model_year: "2021", rom_size: "500gb", ram_size: "16gb")
+Laptop.create(name: "HP Elitebook 820", description: "For those looking for a streamlined, sturdy, and functional notebook PC that won’t disappoint, look no further than the HP EliteBook 820 G1.", image_url: "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/61/965927/1.jpg", price: 6000, model_year: "2021", rom_size: "500gb", ram_size: "4gb")
+Laptop.create(name: "DELL Refurbished Latitude 3150", description: "With its large 500GB hard disk and many options, this gives you the feel of a true portable office.", image_url: "https://ke.jumia.is/unsafe/fit-in/680x680/filters:fill(white)/product/97/3685511/1.jpg?4669", price: 13199.0, model_year: "2022", rom_size: "500", ram_size: "8gb") 
+
+
+
