a60936c1a58f859a1465cf1b4d0f5b7f301b4c17
Harrison Njuguna <noqsnoqs@gmail.com>
2023-07-20 21:14:41 +0300

    Modify migration files

--- db/migrate/20230713124423_create_reservations.rb
+++ db/migrate/20230713124423_create_reservations.rb
@@ -1,11 +1,15 @@
 class CreateReservations < ActiveRecord::Migration[7.0]
   def change
     create_table :reservations do |t|
+      t.integer :user_id
+      t.integer :laptop_id
       t.string :city
-      t.integer :duration_of_hire
-      t.date :date_of_reservation
+      t.date :date
+      t.string :hour
 
       t.timestamps
     end
+
+    add_index :reservations, [:laptop_id, :date, :hour], unique: true
   end
 end
