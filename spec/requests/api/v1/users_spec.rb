a13a82cccefb540b82b9c609b69953119f5bcc6b
QueenterJuma <jumahqueenter@gmail.com>
2023-07-26 18:12:35 -0700

    users_spec.rb

---
+++ spec/requests/api/v1/users_spec.rb
@@ -0,0 +1,23 @@
+require 'swagger_helper'
+
+RSpec.describe 'laptops', type: :request do
+
+  path '/api/v1/signup' do
+    post 'Creates a user' do
+      tags 'Users'
+      consumes 'application/json', 'application/xml'
+      parameter name: :user, in: :body, schema: {
+        type: :object,
+        properties: {
+          username: { type: :string },
+        },
+        required: ['username']
+      }
+
+      response '201', 'User created' do
+        let(:user) { { username: 'test user' } }
+        run_test!
+      end
+    end
+  end
+end
