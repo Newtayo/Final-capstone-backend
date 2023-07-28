141d9dc9902e0e7944759995c1a33acabca2e639
QueenterJuma <jumahqueenter@gmail.com>
2023-07-28 01:27:58 -0700

    Add namespace in route.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
 
-  get 'api/v1/laptops', to: 'laptops#index'
-  post 'api/v1/laptops', to: 'laptops#create'
-  delete 'api/v1/laptops/:id', to: 'laptops#destroy'
-  post 'api/v1/login', to: 'api/v1/users#login'
-  post 'api/v1/signup', to: 'api/v1/users#signup'
-  delete 'api/v1/reservation/:id', to: 'reservations#destroy'
-  post 'api/v1/reservations/', to: 'reservations#create'
-  get 'api/v1/reservations/', to: 'api/v1/reservations#index'
-
+  namespace :api do
+    namespace :v1 do
+      resources :laptops, only: [:index, :create, :destroy]
+      post 'login', to: 'users#login'
+      post 'signup', to: 'users#signup'
+      resources :reservations, only: [:index, :create, :destroy]
+    end
+  end
 end
